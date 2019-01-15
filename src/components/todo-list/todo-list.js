9b277b2b44716bf1f2f32b77439edd64bec19a6c
bvsbvs <bvs0618@gmail.com>
2019-01-15 23:55:01 +0200

    changed components

--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,25 +1,25 @@
 import React from 'react';
 
-import TodoListItem from '../todo-list-item/todo-list-item';
+import TodoListItem from '../todo-list-item';
 import './todo-list.css';
 
 const TodoList = ({ todos }) => {
 
-    const elements = todos.map((item) => {
-        
-        const { id, ...itemProps } = item;
-        return (
-            <li key = {item.id} className="list-group-item">
-                <TodoListItem {...itemProps } />
-            </li>
-        );
-    });
+  const elements = todos.map((item) => {
+    const { id, ...itemProps } = item;
 
     return (
-        <ul className="list-group todo-list">
-            { elements }
-        </ul>
+      <li key={id} className="list-group-item">
+        <TodoListItem {...itemProps } />
+      </li>
     );
+  });
+
+  return (
+    <ul className="list-group todo-list">
+      { elements }
+    </ul>
+  );
 };
 
 export default TodoList;
