7eb9b55fabe52f3ee3509738623ccca910ede6d4
bvsbvs <bvs0618@gmail.com>
2019-01-19 00:15:17 +0200

    added add item button

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,10 +4,13 @@ import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
 import ItemStatusFilter from '../item-status-filter';
+import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
 export default class App extends Component {
+
+  maxId = 100; 
 
   state ={
     todoData: [
@@ -31,6 +34,28 @@ export default class App extends Component {
       }
     })
   };
+
+  addItem = (text) => {
+    // generate id 
+    const newItem = {
+      label: text,
+      important: false,
+      id: this.maxId++
+    }
+
+    // add new element in array
+    this.setState (({ todoData }) => {
+      const newArr = [
+        ...todoData,
+        newItem
+      ];
+
+      return {
+        todoData: newArr
+      }
+    })
+    console.log('Added', text);
+  };
   
   render() {
     return (
@@ -44,6 +69,8 @@ export default class App extends Component {
         <TodoList 
           todos={this.state.todoData} 
           onDeleted={this.deleteItem} />
+
+        <ItemAddForm onItemAdded={this.addItem} />
       </div>
     );
   }
